69d92790ec186ae5bf0d069772a30eb22b0a6e0d
Ben Wilcox <reccanti@gmail.com>
2017-12-29 12:37:00 -0500

    Initial Commit

---
+++ BasicMetalProject/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BasicMetalProject
+//
+//  Created by Benjamin Wilcox on 12/29/17.
+//  Copyright © 2017 Benjamin Wilcox. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BasicMetalProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BasicMetalProject
+//
+//  Created by Benjamin Wilcox on 12/29/17.
+//  Copyright © 2017 Benjamin Wilcox. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
